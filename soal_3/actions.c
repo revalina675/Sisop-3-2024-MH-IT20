b69e7c3ae5241e76e75e23cedb2a2bcfc13c3ad4
revalina675 <150936800+revalina675@users.noreply.github.com>
2024-05-06 02:27:16 +0700

    Update actions.c

--- soal_3/actions.c
+++ soal_3/actions.c
@@ -1,1 +1,41 @@
 
+#include <stdio.h>
+#include <string.h>
+
+char* Gap(float distance) {
+    if (distance < 3.5)
+        return "Gogogo";
+    else if (distance >= 3.5 && distance <= 10)
+        return "Push";
+    else
+        return "Stay out of trouble";
+}
+
+char* Fuel(int percent) {
+    if (percent > 80)
+        return "Push Push Push";
+    else if (percent >= 50 && percent <= 80)
+        return "You can go";
+    else
+        return "Conserve Fuel";
+}
+
+char* Tire(int wear) {
+    if (wear > 80)
+        return "Go Push Go Push";
+    else if (wear >= 50 && wear <= 80)
+        return "Good Tire Wear";
+    else if (wear >= 30 && wear < 50)
+        return "Conserve Your Tire";
+    else
+        return "Box Box Box";
+}
+
+char* TireChange(char* current_type) {
+    if (strcmp(current_type, "Soft") == 0)
+        return "Mediums Ready";
+    else if (strcmp(current_type, "Medium") == 0)
+        return "Box for Softs";
+    else
+        return "Unknown tire type";
+}
